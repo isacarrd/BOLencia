9827969baaa405b8dda9f08eb3241346d0888732
isacarrd <rcpessoaluso@gmail.com>
2026-02-16 01:19:07 -0300

    fixed: ajustando header

--- src/navigation/AuthStack.tsx
+++ src/navigation/AuthStack.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React from 'react';
 /* import LoginScreen from '../screens/auth/Login'; */
-import RegisterScreen from '../screens/auth/Register';
+import LoginScreen from '../screens/auth/Login';
 import WelcomeScreen from '../screens/auth/Welcome';
+import { theme } from '../theme';
 import { AuthStackParamList } from './index';
 
 const Auth = createNativeStackNavigator<AuthStackParamList>();
@@ -10,7 +11,24 @@ const Auth = createNativeStackNavigator<AuthStackParamList>();
 export default function AuthStack() {
   return (
     <Auth.Navigator>
-      <Auth.Screen name='Welcome' component={WelcomeScreen}/>
+      <Auth.Screen
+        name="Welcome"
+        component={WelcomeScreen}
+        options={{
+          headerStyle: { backgroundColor: theme.colors.preto },
+          headerTintColor: theme.colors.branco,
+          title: '',
+        }}
+      />
+      <Auth.Screen
+        name="Login"
+        component={LoginScreen}
+        options={{
+          headerStyle: { backgroundColor: theme.colors.preto },
+          headerTintColor: theme.colors.branco,
+          title: '',
+        }}
+      />
     </Auth.Navigator>
   );
 }
