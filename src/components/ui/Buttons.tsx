15dc29d514a62bba91eb9c4acdf7ab2a160bd336
isacarrd <rcpessoaluso@gmail.com>
2026-02-15 23:23:38 -0300

    update: criação de componente BOTÃO

--- src/components/ui/Buttons.tsx
+++ src/components/ui/Buttons.tsx
@@ -1,29 +1,59 @@
 import React from 'react';
-import { TouchableOpacity, Text, StyleSheet, ViewStyle } from 'react-native';
+import { TouchableOpacity, Text, StyleSheet, ViewStyle, type GestureResponderEvent } from 'react-native';
 import { theme } from '../../theme';
 
+type ButtonProps = {
+  title: string;
+  onPress: (event: GestureResponderEvent) => void;
+  type?: 'filled' | 'outlined'; // tipos de botão
+};
 
-interface BotaoProps {
-  titulo: string;
-  aoPressionar: () => void;
-  corDeFundo?: string;
-  corDeBorda?: string;
-}
-
-const MeuBotao = ({
-  titulo,
-  aoPressionar,
-  corDeFundo = '#007BFF',
-}: BotaoProps) => {
+const CustomButton: React.FC<ButtonProps> = ({ 
+  title, 
+  onPress, 
+  type = 'filled' 
+}) => {
   return (
     <TouchableOpacity
-      style={[]}
-      onPress={aoPressionar}
-      activeOpacity={0.7}
+      style={[
+        styles.button,
+        type === 'filled' ? styles.filled : styles.outlined
+      ]}
+      onPress={onPress}
     >
-      <Text ></Text>
+      <Text
+        style={[
+          styles.text
+        ]}
+      >
+        {title}
+      </Text>
     </TouchableOpacity>
   );
 };
 
-export default MeuBotao;
+const styles = StyleSheet.create({
+  button: {
+    paddingVertical: 12,
+    paddingHorizontal: 20,
+    borderRadius: 8,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginVertical: 8,
+  },
+  filled: {
+    backgroundColor: theme.colors.roxoPadrao,
+  },
+  outlined: {
+    borderWidth: 2,
+    borderColor: theme.colors.roxoPadrao,
+    backgroundColor: 'transparent',
+  },
+  text: {
+    fontSize: theme.size.mdX,
+    fontFamily: theme.fonts.bold,
+    color: theme.colors.branco
+  },
+});
+
+export default CustomButton;
